fbf1e15c1cc8f16fbfd6cd11ce658e3464505a1a
alanpcao@gmail.com <alanpcao@gmail.com>
2020-07-10 10:04:57 -0700

    Finish Parstagram Pt.1

--- Parstagram-CodePath-2020/FeedViewController.swift
+++ Parstagram-CodePath-2020/FeedViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import Parse
 
 class FeedViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    private var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if let error = error {
+                print("DEBUG: \(error.localizedDescription)")
+                return
+            }
+            self.posts = posts!
+            self.tableView.reloadData()
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -27,4 +50,25 @@ class FeedViewController: UIViewController {
     }
     */
 
+}
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        let post = posts[indexPath.row] 
+        cell.usernameLabel.text = post["author"] as? String ?? ""
+        cell.captionLabel.text = post["caption"] as? String ?? ""
+        
+        let imageFile = post["image"] as! PFFileObject
+        let URLString = imageFile.url!
+        let url = URL(string: URLString)
+        
+        cell.postImageView.af_setImage(withURL: url!)
+        
+        return cell
+    }
 }

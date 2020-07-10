06a13b2b729bb6e69e7f1e675bcdb08391c26626
alanpcao@gmail.com <alanpcao@gmail.com>
2020-07-10 09:23:45 -0700

    Add xcassets and complete sign in landing view

---
+++ Parstagram-CodePath-2020/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  Parstagram-CodePath-2020
+//
+//  Created by Alan Cao on 7/10/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func handleSignIn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func handleSignUp(_ sender: Any) {
+        
+    }
+}

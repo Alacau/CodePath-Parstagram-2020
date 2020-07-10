d8167b6da57e01b4ddcfa7d3f9b75d2366c523ef
alanpcao@gmail.com <alanpcao@gmail.com>
2020-07-10 09:07:50 -0700

    Add project files

---
+++ Parstagram-CodePath-2020Tests/Parstagram_CodePath_2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Parstagram_CodePath_2020Tests.swift
+//  Parstagram-CodePath-2020Tests
+//
+//  Created by Alan Cao on 7/10/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import XCTest
+@testable import Parstagram_CodePath_2020
+
+class Parstagram_CodePath_2020Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

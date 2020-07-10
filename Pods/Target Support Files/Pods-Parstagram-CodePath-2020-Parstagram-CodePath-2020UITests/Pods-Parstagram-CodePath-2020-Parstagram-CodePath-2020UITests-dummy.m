f37629a37370c65f2ecc7412ea16d7ad716892be
alanpcao@gmail.com <alanpcao@gmail.com>
2020-07-10 09:12:32 -0700

    Add pods

---
+++ Pods/Target Support Files/Pods-Parstagram-CodePath-2020-Parstagram-CodePath-2020UITests/Pods-Parstagram-CodePath-2020-Parstagram-CodePath-2020UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Parstagram_CodePath_2020_Parstagram_CodePath_2020UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Parstagram_CodePath_2020_Parstagram_CodePath_2020UITests
+@end
